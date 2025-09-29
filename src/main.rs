a455f220977c84e175cbaa3bffe67ef4797be4b4
Alireza Gharib <alirezagharib110@gmail.com>
2025-09-29 20:52:17 +0330

    project is completed

--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,342 @@
-use std::net::{TcpListener, TcpStream};
-use std::convert::TryFrom;
+use std::{env, fs};
+use std::collections::HashMap;
 use std::error::Error;
-use std::fmt::{write, Debug, Display, Formatter};
-use std::io::{Read, };
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::io::{Read, Write, Result as IoResult};
+use std::net::TcpListener;
+use std::str::{FromStr, Utf8Error};
 
-struct Server {
-    address: String,
+#[derive(Debug)]
+pub struct Request<'buf> {
+    path: &'buf str,
+    query_string: Option<QueryString<'buf>>,
+    method: Method,
 }
 
-impl Server {
-    fn new(address: String) -> Server {
-        Server { address }
+impl<'buf> Request<'buf> {
+    pub fn path(&self) -> &str {
+        &self.path
     }
 
-    fn run(self) {
-        println!("Server running on {}", self.address);
-        let listener = TcpListener::bind(&self.address).unwrap();
+    pub fn method(&self) -> &Method {
+        &self.method
+    }
 
-        loop {
-            match listener.accept() {
-                Ok((mut stream, addrSock)) => {
-                    let mut buffer = [0; 1024];
-                    match stream.read(&mut buffer) {
-                        Ok(_) => {
-                            println!("Request Msg Received: {}",
-                                     String::from_utf8_lossy(&buffer[..]));
-                            //Request::try_from(&buffer as &[u8]).unwrap(); // Tell the compiler treat &[u8;1024] as &[u8]
-                            match Request::try_from(&buffer[..]) {
-                                Ok(req) => {}
-                                Err(e) => {println!("Failed to parse request: {}", e);}
-                            }
-                        }
+    pub fn query_string(&self) -> Option<&QueryString> {
+        self.query_string.as_ref()
+    }
+}
 
-                        Err(e) => {
-                            println!("Error reading from stream: {}", e);
-                        }
-                    }
-                }
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
+    type Error = ParseError;
 
-                Err(e) => {
-                    println!("Error accepting connection: {}", e);
-                }
-            }
+    // GET /search?name=abc&sort=1 HTTP/1.1\r\n...HEADERS...
+    fn try_from(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
+        let request = str::from_utf8(buf)?;
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
+
+        let mut query_string = None;
+        if let Some(i) = path.find('?') {
+            query_string = Some(QueryString::from(&path[i + 1..]));
+            path = &path[..i];
+        }
+
+        Ok(Self {
+            path,
+            query_string,
+            method,
+        })
+    }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+
+    None
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum StatusCode {
+    Ok = 200,
+    BadRequest = 400,
+    NotFound = 404,
+}
+
+impl StatusCode {
+    pub fn reason_phrase(&self) -> &str {
+        match self {
+            Self::Ok => "Ok",
+            Self::BadRequest => "Bad Request",
+            Self::NotFound => "Not Found",
         }
     }
 }
 
-struct Request {
-    path: String,
-    method: Method,
-    query_string: Option<String>,
-}
-
-impl Request {
-    fn from_byte_array(buf: &[u8]) -> Result<Request, String> {
-        unimplemented!()
+impl Display for StatusCode {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", *self as u16)
     }
 }
 
-impl TryFrom<&[u8]> for Request {
-    type Error = ParseError;
+#[derive(Debug)]
+pub struct Response {
+    status_code: StatusCode,
+    body: Option<String>,
+}
 
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        unimplemented!()
+impl Response {
+    pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
+        Response { status_code, body }
+    }
+
+    pub fn send(&self, stream: &mut impl Write) -> IoResult<()> {
+        let body = match &self.body {
+            Some(b) => b,
+            None => "",
+        };
+
+        write!(
+            stream,
+            "HTTP/1.1 {} {}\r\n\r\n{}",
+            self.status_code,
+            self.status_code.reason_phrase(),
+            body
+        )
     }
 }
 
-enum Method {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    PATCH,
-    OPTIONS,
-    HEAD,
-    TRACE,
-    CONNECT,
-}
-
-
-enum ParseError {
-    InvalidMethod,
-    InvalidQuery,
-    InvalidHeader,
+pub enum ParseError {
     InvalidRequest,
     InvalidEncoding,
     InvalidProtocol,
+    InvalidMethod,
 }
 
 impl ParseError {
     fn message(&self) -> &str {
         match self {
-            ParseError::InvalidMethod => "Invalid method",
-            ParseError::InvalidQuery => "Invalid query",
-            ParseError::InvalidHeader => "Invalid header",
-            ParseError::InvalidRequest => "Invalid request",
-            ParseError::InvalidEncoding => "Invalid encoding",
-            ParseError::InvalidProtocol => "Invalid protocol",
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
         }
     }
 }
 
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
 impl Display for ParseError {
-    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
         write!(f, "{}", self.message())
     }
 }
 
 impl Debug for ParseError {
-    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
         write!(f, "{}", self.message())
     }
 }
 
-impl Error for ParseError {
+impl Error for ParseError {}
 
+#[derive(Debug)]
+pub struct QueryString<'buf> {
+    data: HashMap<&'buf str, Value<'buf>>,
+}
+
+#[derive(Debug)]
+pub enum Value<'buf> {
+    Single(&'buf str),
+    Multiple(Vec<&'buf str>),
+}
+
+impl<'buf> QueryString<'buf> {
+    pub fn get(&self, key: &str) -> Option<&Value> {
+        self.data.get(key)
+    }
+}
+
+// a=1&b=2&c&d=&e===&d=7&d=abc
+impl<'buf> From<&'buf str> for QueryString<'buf> {
+    fn from(s: &'buf str) -> Self {
+        let mut data = HashMap::new();
+
+        for sub_str in s.split('&') {
+            let mut key = sub_str;
+            let mut val = "";
+            if let Some(i) = sub_str.find('=') {
+                key = &sub_str[..i];
+                val = &sub_str[i + 1..];
+            }
+
+            data.entry(key)
+                .and_modify(|existing: &mut Value| match existing {
+                    Value::Single(prev_val) => {
+                        *existing = Value::Multiple(vec![prev_val, val]);
+                    }
+                    Value::Multiple(vec) => vec.push(val),
+                })
+                .or_insert(Value::Single(val));
+        }
+
+        QueryString { data }
+    }
+}
+
+
+#[derive(Debug)]
+pub enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
+
+impl FromStr for Method {
+    type Err = MethodError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "GET" => Ok(Self::GET),
+            "DELETE" => Ok(Self::DELETE),
+            "POST" => Ok(Self::POST),
+            "PUT" => Ok(Self::PUT),
+            "HEAD" => Ok(Self::HEAD),
+            "CONNECT" => Ok(Self::CONNECT),
+            "OPTIONS" => Ok(Self::OPTIONS),
+            "TRACE" => Ok(Self::TRACE),
+            "PATCH" => Ok(Self::PATCH),
+            _ => Err(MethodError),
+        }
+    }
+}
+
+pub struct MethodError;
+
+pub struct WebsiteHandler {
+    public_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Traversal Attack Attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
+    }
+}
+
+impl Handler for WebsiteHandler {
+    fn handle_request(&mut self, request: &Request) -> Response {
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
+            },
+            _ => Response::new(StatusCode::NotFound, None),
+        }
+    }
+}
+
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+
+    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+        println!("Failed to parse request: {}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
+}
+
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    pub fn run(self, mut handler: impl Handler) {
+        println!("Listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            let response = match Request::try_from(&buffer[..]) {
+                                Ok(request) => handler.handle_request(&request),
+                                Err(e) => handler.handle_bad_request(&e),
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
+                            }
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
+        }
+    }
 }
 
 
 fn main() {
-    //let addr = String::from("127.0.0.1:8080");
-    let srv = Server::new("127.0.0.1:9980".to_string());
-    srv.run();
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("public path: {}", public_path);
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run(WebsiteHandler::new(public_path));
 }
