9efb9eb33602427387405abd8d8731d4c8a87777
Alireza Gharib <alirezagharib110@gmail.com>
2025-09-29 13:07:14 +0330

    ParseError is implemented for using in Request TryFrom implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::net::{TcpListener, TcpStream};
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{write, Debug, Display, Formatter};
 use std::io::{Read, };
+
 struct Server {
     address: String,
 }
@@ -21,6 +25,11 @@ impl Server {
                         Ok(_) => {
                             println!("Request Msg Received: {}",
                                      String::from_utf8_lossy(&buffer[..]));
+                            //Request::try_from(&buffer as &[u8]).unwrap(); // Tell the compiler treat &[u8;1024] as &[u8]
+                            match Request::try_from(&buffer[..]) {
+                                Ok(req) => {}
+                                Err(e) => {println!("Failed to parse request: {}", e);}
+                            }
                         }
 
                         Err(e) => {
@@ -43,6 +52,20 @@ struct Request {
     query_string: Option<String>,
 }
 
+impl Request {
+    fn from_byte_array(buf: &[u8]) -> Result<Request, String> {
+        unimplemented!()
+    }
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
+
 enum Method {
     GET,
     POST,
@@ -54,6 +77,46 @@ enum Method {
     TRACE,
     CONNECT,
 }
+
+
+enum ParseError {
+    InvalidMethod,
+    InvalidQuery,
+    InvalidHeader,
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            ParseError::InvalidMethod => "Invalid method",
+            ParseError::InvalidQuery => "Invalid query",
+            ParseError::InvalidHeader => "Invalid header",
+            ParseError::InvalidRequest => "Invalid request",
+            ParseError::InvalidEncoding => "Invalid encoding",
+            ParseError::InvalidProtocol => "Invalid protocol",
+        }
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {
+
+}
+
 
 fn main() {
     //let addr = String::from("127.0.0.1:8080");
